657048d78b1e30fd76ac7a9c769b07a93913efb5
“honue” <zouhy2001@gmail.com>
2023-03-20 19:31:09 +0800

    自定义订阅支持使用代理

--- web/action.py
+++ web/action.py
@@ -2642,6 +2642,7 @@ class WebAction:
             "state": data.get("state"),
             "save_path": data.get("save_path"),
             "download_setting": data.get("download_setting"),
+            "note": data.get("note"),
         }
         if uses == "D":
             params.update({
